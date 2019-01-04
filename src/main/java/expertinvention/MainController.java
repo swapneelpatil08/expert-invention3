f0e1229756f9bd98ef05f6d9940347256fc7720e
Swapneel Patil <swapneel.patil08@gmail.com>
2019-01-05 00:03:23 +0530

    Added Project.

---
+++ src/main/java/expertinvention/MainController.java
@@ -0,0 +1,14 @@
+package expertinvention;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String index(){
+
+        return "Welcome page.";
+    }
+}
